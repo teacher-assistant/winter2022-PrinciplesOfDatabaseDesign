a17548f5841ee6e4b817f18d8f0c74b786415b79
Omid Reza <omid77.orh@gmail.com>
2022-04-11 15:41:16 +0430

    add icon into title of pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/winter2022-PrinciplesOfDatabaseDesign/icon.svg'}
     ]
   },
 
